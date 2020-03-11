f12b9ffd487323c456ef95b2506d0efd1a10a8ec
kingToby1 <62064921+kingToby1@users.noreply.github.com>
2020-03-11 11:24:28 -0600

    if you are a beginner and you want to learn python this is the file for you!
    if you wan't to learn more, go to my youtube channel at https://www.youtube.com/channel/UCKG3RxaQxM_lG0R5vNZrdRg

---
+++ PythonTutorial1.py
@@ -0,0 +1,17 @@
+#variables are numbers or strings that you can assign to different variables.
+#example
+x = 'hello'
+print(x)
+#in this example x is the variable that is equal to the string 'hello' when you say print(x) it's really saying
+# print('hello')
+#example2
+z = 'world'
+y = x+z
+print(y)
+#in this example it is saying that y is equal to the value x+z. z is equal to 'hello' and x is equal to 'world' so in this 
+#case it would be 'hello' + 'world' which is equal to 'helloworld' so y = 'helloworld'. the reason it won't do spaces is 
+#because you never put in spaces so instead of making z 'world', you can modify it to make ' world' now hit run. 
+#It should now look like this >>>hello world or >>>helloworld depending on if you put a space on the 'world' string or not.
+
+
+#means comment if your a begginer :)
